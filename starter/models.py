4308fa3d509694556f125d796e649c28c3ad3d9d
Eka Kurniawan <eka.a.kurniawan@gmail.com>
2020-03-11 09:30:02 +0700

    Implement NearEarthObject and OrbitPath Classes - Add userful variables - Print object information

--- starter/models.py
+++ starter/models.py
@@ -10,6 +10,28 @@ class NearEarthObject(object):
         :param kwargs:    dict of attributes about a given Near Earth Object, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.id = kwargs['id']
+        self.neo_reference_id = kwargs['neo_reference_id']
+        self.name = kwargs['name']
+        self.nasa_jpl_url = kwargs['nasa_jpl_url']
+        self.absolute_magnitude_h = float(kwargs['absolute_magnitude_h'])
+        self.estimated_diameter_min_kilometers = float(kwargs['estimated_diameter_min_kilometers'])
+        self.estimated_diameter_max_kilometers = float(kwargs['estimated_diameter_max_kilometers'])
+        self.estimated_diameter_min_meters = float(kwargs['estimated_diameter_min_meters'])
+        self.estimated_diameter_max_meters = float(kwargs['estimated_diameter_max_meters'])
+        self.estimated_diameter_min_miles = float(kwargs['estimated_diameter_min_miles'])
+        self.estimated_diameter_max_miles = float(kwargs['estimated_diameter_max_miles'])
+        try:
+            self.estimated_diameter_min_feet = float(kwargs['estimated_diameter_min_feet'])
+        except:
+            self.estimated_diameter_min_feet = None
+        self.estimated_diameter_max_feet = float(kwargs['estimated_diameter_max_feet'])
+        self.is_potentially_hazardous_asteroid = bool(kwargs['is_potentially_hazardous_asteroid'])
+        self.__orbits = []
+
+    @property
+    def orbits(self):
+        return self.__orbits
 
     def update_orbits(self, orbit):
         """
@@ -18,8 +40,11 @@ class NearEarthObject(object):
         :param orbit: OrbitPath
         :return: None
         """
-
         # TODO: How do we connect orbits back to the Near Earth Object?
+        self.__orbits.append(orbit)
+
+    def __repr__(self):
+        return self.name
 
 
 class OrbitPath(object):
@@ -34,3 +59,16 @@ class OrbitPath(object):
         :param kwargs:    dict of attributes about a given orbit, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.kilometers_per_second = float(kwargs['kilometers_per_second'])
+        self.kilometers_per_hour = float(kwargs['kilometers_per_hour'])
+        self.miles_per_hour = float(kwargs['miles_per_hour'])
+        self.close_approach_date = kwargs['close_approach_date']
+        self.close_approach_date_full = kwargs['close_approach_date_full'] 
+        self.miss_distance_astronomical = float(kwargs['miss_distance_astronomical'])
+        self.miss_distance_lunar = float(kwargs['miss_distance_lunar'])
+        self.miss_distance_kilometers = float(kwargs['miss_distance_kilometers'])
+        self.miss_distance_miles = float(kwargs['miss_distance_miles'])
+        self.orbiting_body = kwargs['orbiting_body']
+
+    def __repr__(self):
+        return self.close_approach_date
